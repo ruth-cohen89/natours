dbc8ee3a58d6720c55c999ac6fea6d9bf28dae0d
ruth-cohen89 <57313608+ruth-cohen89@users.noreply.github.com>
2022-06-16 00:01:02 +0300

    all set

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -9,7 +9,7 @@ export const bookTour = async (tourId) => {
     const session = await axios(
         `/api/v1/bookings/checkout-session/${tourId}`
     );
-  
+    
     await stripe.redirectToCheckout({
         sessionId: session.data.session.id
     });
